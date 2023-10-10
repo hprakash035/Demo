a3cc28cd3d26898945819d39bb79172cc4acb28d
Himanshu Prakash <himanshu.shila035@gmail.com>
2023-10-10 17:06:59 +0000

    initial changes

---
+++ srv/service.js
@@ -0,0 +1,11 @@
+const cds = require('@sap/cds'); 
+module.exports = cds.service.impl(async function () {
+this.on('resolve', async (req) => {
+const tx = cds.tx(req);
+await tx.run(
+UPDATE('Escalations')
+.set({ 'Status_code': 'CMP' })
+.where({ ID: req.params[0].ID })
+);
+})
+});
